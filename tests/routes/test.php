7907dab36703e6c94e3a1f6a2135188482fe54a3
Piiotr Krzempek <johnylemon1989@gmail.com>
2020-12-10 23:48:44 +0100

    composer versions updated

--- tests/routes/test.php
+++ tests/routes/test.php
@@ -2,10 +2,10 @@
 
 use Johnylemon\Apidocs\Tests\Data\TestEndpoint;
 
-Route::get('test', fn() => [])->name('test')->apidocs(TestEndpoint::class);
-Route::get('anothertest', fn() => [])->name('anothertest')->apidocs(TestEndpoint::class);
+Route::get('test', function() {})->name('test')->apidocs(TestEndpoint::class);
+Route::get('anothertest', function() { return []; })->name('anothertest')->apidocs(TestEndpoint::class);
 
-Route::get('test1', fn() => [])->name('test1');
-Route::get('test2', fn() => [])->name('test2');
-Route::get('test3', fn() => [])->name('test3');
-Route::get('test4', fn() => [])->name('test4');
+Route::get('test1', function() { return []; })->name('test1');
+Route::get('test2', function() { return []; })->name('test2');
+Route::get('test3', function() { return []; })->name('test3');
+Route::get('test4', function() { return []; })->name('test4');
